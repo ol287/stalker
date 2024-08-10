04c3590795e6b4263aa1d0dd4142eef095dcdb5d
ol287 <ol287@exeter.ac.uk>
2024-08-10 23:05:06 +0100

    calculate dis

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def printDetails(ip):
     print(f"IP Address: {res.ip_address}")
     print(f"Location: {res.city}, {res.region}, {res.country}")
     print(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
+    
+def calculate_distance(ip1, ip2):
+    res1 = DbIpCity.get(ip1)
+    res2 = DbIpCity.get(ip2)
+    lat1, lon1 = res1.latitude, res1.longitude
+    lat2, lon2 = res2.latitude, res2.longitude
+    return distance((lat1, lon1), (lat2, lon2)).km
 
 #ip to geo
 ip_add = input("Enter IP: ")  # 198.35.26.96
