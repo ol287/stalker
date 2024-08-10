08296cf58a8da585701175535d12a8c97bcfe321
ol287 <ol287@exeter.ac.uk>
2024-08-10 23:04:18 +0100

    url to geo

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def printDetails(ip):
     print(f"IP Address: {res.ip_address}")
     print(f"Location: {res.city}, {res.region}, {res.country}")
     print(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
-    
+
+#ip to geo
 ip_add = input("Enter IP: ")  # 198.35.26.96
+printDetails(ip_add)
+
+
+#url to geo 
+url = input("Enter URL: ")  # www.youtube.com
+ip_add = socket.gethostbyname(url)
 printDetails(ip_add)
