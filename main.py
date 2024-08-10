c6dee8573856fc3f458482b7ed529e75aa057507
ol287 <ol287@exeter.ac.uk>
2024-08-10 23:03:30 +0100

    ip address translated to coordinates

---
+++ main.py
@@ -0,0 +1,13 @@
+import socket
+import requests
+from ip2geotools.databases.noncommercial import DbIpCity
+from geopy.distance import distance
+
+def printDetails(ip):
+    res = DbIpCity.get(ip, api_key="free")
+    print(f"IP Address: {res.ip_address}")
+    print(f"Location: {res.city}, {res.region}, {res.country}")
+    print(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
+    
+ip_add = input("Enter IP: ")  # 198.35.26.96
+printDetails(ip_add)
